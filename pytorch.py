c724d8b0dde39e56647e117c91cd446b3f1db0a9
Jaisel Singh <singhjaisel01@gmail.com>
2025-12-23 22:23:29 -0800

    adding eval

--- pytorch.py
+++ pytorch.py
@@ -54,6 +54,8 @@ class NeuralNetwork(nn.Module):
         self.linear_relu_stack == nn.Sequential(
             nn.Linear(28 * 28, 512),
             nn.ReLU(), 
+            # the ReLU activation function is defined as being f(x) = max(0, x)
+            # the tanh() activation would bound it between -1 and 1 for the ouptput
             nn.Linear(512, 512), 
             nn.ReLU(), 
             nn.Linear(512, 10)
@@ -62,6 +64,76 @@ class NeuralNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+    
+    # logits are raw numbers that are not converted into probabilities by being passed into a softmax function (or some other activation for the final layer)
+    # the reLU is the activation in the middle for learning non linearities 
 
 model = NeuralNetwork().to(device)
 print(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    # in the training loop, the model does predictions on the training dataset and then backprops the pred error
+    # this error is used to make updates to the params in the batch_size 
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (x,y) in enumerate(dataloader):
+        x, y = x.to(device), y.to(device)
+
+        # compute the prediction error 
+        pred = model(x)
+        loss = loss_fn(pred, y)
+
+        # backprop 
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(x)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
