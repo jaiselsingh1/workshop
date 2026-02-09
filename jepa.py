8fdf25ec02c4de96015f1bcf1de448bbb5b0a760
Jaisel Singh <singhjaisel01@gmail.com>
2026-02-09 00:16:43 -0500

    adding patch

--- jepa.py
+++ jepa.py
@@ -2,3 +2,45 @@ import math
 import random 
 from dataclasses import dataclass 
 from typing import List, Tuple, Optional
+
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F
+
+# images have shape (B, C, H, W)
+# B = batch size, C = channels, H = height, W = width
+# patch tokens have shape (B, N, D) where N = number of patches, D = embedding dimension 
+
+
+class PatchEmbed(nn.Module):
+    """converts the image into a sequence of patch embeddings
+    Input = (B , C, H, W)
+    Ouput = (B, N, D) where N = (H/P) * (W/P)
+    """
+
+    def __init__(
+            self, 
+            img_size: int = 224, 
+            patch_size: int = 16, 
+            in_chans: int = 3, 
+            embed_dim: int = 768, # the dimension of the embedding that represents each patch 
+    ):
+        super().__init__()
+        assert img_size % patch_size == 0, "img size must be dividable by the patch size"
+        self.img_size = img_size 
+        self.patch_size = patch_size
+        self.grid_size = img_size // patch_size
+        self.num_patches = self.grid_size * self.grid_size
+
+        self.proj == nn.Conv2d(
+            in_channels = in_chans, 
+            out_channels = embed_dim, 
+            stride = patch_size, 
+            kernel_size = patch_size
+            bias = True
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.proj(x) # (B,D,G,G)
+        x = x.flatten(2).transpose(1, 2) # (B, N, D)
+        return x
